76ac1f243d4bb78a50f28c4c0437ceffe5653ede
santiagourregobotero <santiagourregobotero@gmail.com>
2024-01-12 05:49:14 +0900

    update config for the resume resource

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,7 +1,6 @@
 export const AppConfig = {
-  /*
   resumeBaseUrl:
     'https://raw.githubusercontent.com/santiagourregobotero/santiagourregobotero/master/RESUME/',
-    */
-  resumeBaseUrl: 'http://localhost:4200/assets/RESUME',
+
+  // resumeBaseUrl: 'http://localhost:4200/assets/RESUME',
 };
